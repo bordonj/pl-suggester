481ed5b09b86f19b9c44a0ef624ccbeec57650d2
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-04-29 17:59:22 -0700

    add the jQuery transitions to next question sets

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,24 @@ $(document).ready(function() {
     $('.intro').hide();
     $('#firstSet').fadeIn();
   })
+  $('#first').submit(function(e2) {
+    e2.preventDefault();
+    $('#firstSet').hide();
+    $('#secondSet').fadeIn();
+  })
+  $('#second').submit(function(e3) {
+    e3.preventDefault();
+    $('#secondSet').hide();
+    $('#thirdSet').fadeIn();
+  })
+  $('#third').submit(function(e3) {
+    e3.preventDefault();
+    $('#thirdSet').hide();
+    $('#fourthSet').fadeIn();
+  })
+  $('#fourth').submit(function(e3) {
+    e3.preventDefault();
+    $('#fourthSet').hide();
+    $('#fifthSet').fadeIn();
+  })
 })
